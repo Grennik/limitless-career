ab1dde701b9cec5f25584a2478af580aec92acec
Grennik <joey.ciervo@gmail.com>
2015-09-10 20:54:47 -0400

    completed - check if any changes need made

--- js/main.js
+++ js/main.js
@@ -1,1 +1,55 @@
+jQuery(function($){
+  $("#clarity-btn").click(function(e){
+    $("#clarity-dropdown").toggleClass("hidden");
+    e.preventDefault();
+  });
+});
 
+jQuery(function($){
+  $("#elevator-btn").click(function(f){
+    $("#elevator-dropdown").toggleClass("hidden");
+    f.preventDefault();
+  });
+});
+
+jQuery(function($){
+  $("#resume-btn").click(function(g){
+    $("#resume-dropdown").toggleClass("hidden");
+    g.preventDefault();
+  });
+});
+
+jQuery(function($){
+  $("#cover-letter-btn").click(function(h){
+    $("#cover-letter-dropdown").toggleClass("hidden");
+    h.preventDefault();
+  });
+});
+
+jQuery(function($){
+  $("#networking-btn").click(function(i){
+    $("#networking-dropdown").toggleClass("hidden");
+    i.preventDefault();
+  });
+});
+
+jQuery(function($){
+  $("#cold-btn").click(function(j){
+    $("#cold-dropdown").toggleClass("hidden");
+    j.preventDefault();
+  });
+});
+
+jQuery(function($){
+  $("#interview-btn").click(function(k){
+    $("#interview-dropdown").toggleClass("hidden");
+    k.preventDefault();
+  });
+});
+
+jQuery(function($){
+  $("#salary-btn").click(function(l){
+    $("#salary-dropdown").toggleClass("hidden");
+    l.preventDefault();
+  });
+});
